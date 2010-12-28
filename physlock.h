636e4c5bba9e79220d5a1a0c5d6316f645b95ef8
Bert <ber.t@gmx.com>
2010-12-28 15:11:31 +0100

    use perror in WARN and FATAL

--- physlock.h
+++ physlock.h
@@ -3,16 +3,18 @@
 
 #define CONSOLE_DEV "/dev/console"
 
-#define WARN(msg)                                    \
-	fprintf(stderr, "physlock: %s:%d: warning: %s\n",  \
-			__FILE__, __LINE__, msg);
+#define WARN(msg)                                                      \
+	do {                                                                 \
+		fprintf(stderr, "physlock: %s:%d: warning: ", __FILE__, __LINE__); \
+		perror(msg);                                                       \
+	} while (0)
 
-#define FATAL(msg)                                   \
-  do {                                               \
-		fprintf(stderr, "physlock: %s:%d: error: %s\n",  \
-		    __FILE__, __LINE__, msg);                    \
-		cleanup(0);                                      \
-		exit(1);                                         \
+#define FATAL(msg)                                                     \
+  do {                                                                 \
+		fprintf(stderr, "physlock: %s:%d: error: ", __FILE__, __LINE__);   \
+		perror(msg);                                                       \
+		cleanup(0);                                                        \
+		exit(1);                                                           \
 	} while (0)
 
 void cleanup(int warn);
