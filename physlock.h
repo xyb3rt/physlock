001224c59a72a58f363edeadda6097331395adde
Bert <ber.t@gmx.com>
2011-01-03 16:18:22 +0100

    unstable version number

--- physlock.h
+++ physlock.h
@@ -30,7 +30,7 @@
 
 #include "config.h"
 
-#define VERSION "0.1"
+#define VERSION "git-20110103"
 
 #define WARN(...)                                                      \
 	do {                                                                 \
