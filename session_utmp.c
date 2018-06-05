b75b6539bd0a224043d8a9e2506103a1848c5769
Bert Münnich <ber.t@posteo.de>
2018-06-05 14:43:14 +0200

    Revert "Fix strcmp on utmp's ut_user string"
    This reverts commit 23227f51eab447055f21a0baa1af926c5eb3730f.

--- session_utmp.c
+++ session_utmp.c
@@ -9,11 +9,11 @@
 #include <utmp.h>
 
 void get_user(userinfo_t *uinfo, int vt, uid_t owner) {
-	int found = 0;
 	FILE *uf;
 	struct utmp r;
 	char tty[16], name[UT_NAMESIZE+1];
 
+	name[0] = '\0';
 	while ((uf = fopen(_PATH_UTMP, "r")) == NULL && errno == EINTR);
 
 	if (uf != NULL) {
@@ -23,16 +23,16 @@ void get_user(userinfo_t *uinfo, int vt, uid_t owner) {
 				continue;
 			if (r.ut_type != USER_PROCESS || r.ut_user[0] == '\0')
 				continue;
-			strncpy(name, r.ut_user, UT_NAMESIZE);
-			name[UT_NAMESIZE] = '\0';
-			if (strcmp(name, tty) == 0) {
-				found = 1;
+			if (strcmp(r.ut_line, tty) == 0) {
+				strncpy(name, r.ut_user, UT_NAMESIZE);
+				name[UT_NAMESIZE] = '\0';
 				break;
 			}
 		}
 		fclose(uf);
 	}
-	if (found)
+
+	if (name[0] != '\0')
 		get_user_by_name(uinfo, name);
 	else
 		error(EXIT_FAILURE, 0, "No utmp entry for tty%d found", vt);
